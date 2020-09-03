2b625cef9c22510585626c85af850625240cbef4
Marcin <marcin9213@gmail.com>
2020-09-03 22:12:21 +0200

    organize code

--- js/index.js
+++ js/index.js
@@ -1,49 +1,29 @@
-const FULL_DASH_ARRAY = 283;
-const WARNING_THRESHOLD = 10;
-const ALERT_THRESHOLD = 5;
-
-const COLOR_CODES = {
-  info: {
-    color: 'green',
-  },
-  warning: {
-    color: 'orange',
-    threshold: WARNING_THRESHOLD,
-  },
-  alert: {
-    color: 'red',
-    threshold: ALERT_THRESHOLD,
-  },
-};
-
-let timerInterval = null;
-let remainingPathColor = COLOR_CODES.info.color;
-
-document.querySelector('.timer__clock').innerHTML = `
-
-  <svg class="clock__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-    <g class="clock__circle">
-      <circle class="clock__path-elapsed" cx="50" cy="50" r="45"></circle>
-      <path
-        id="clock-path-remaining"
-        stroke-dasharray="283"
-        class="clock__path-remaining ${remainingPathColor}"
-        d="
-          M 50, 50
-          m -45, 0
-          a 45,45 0 1,0 90,0
-          a 45,45 0 1,0 -90,0
-        "
-      ></path>
-    </g>
-  </svg>
-  <span id="clock-label" class="clock__label">0:00</span>
-
-`;
-
 document.querySelector('.timer__icon').addEventListener('click', () => {
-  let timeLimit = document.querySelector('.display__input').value;
+  let timeLimit = Number(document.querySelector('.display__input').value) * 60;
   let timeLeft = timeLimit;
+  let WARNING_THRESHOLD = timeLimit / 2;
+  let ALERT_THRESHOLD = timeLimit / 5;
+  const FULL_DASH_ARRAY = 283;
+  let timerInterval = null;
+
+  const COLOR_CODES = {
+    info: {
+      color: 'green',
+    },
+    warning: {
+      color: 'orange',
+      threshold: WARNING_THRESHOLD,
+    },
+    alert: {
+      color: 'red',
+      threshold: ALERT_THRESHOLD,
+    },
+  };
+
+  document.getElementById('clock-label').innerHTML = formatTime(timeLeft);
+  document.querySelector('.clock__label--timer').classList.remove('clock__label--timer');
+  document.querySelector('.clock__setTime').classList.add('clock__setTime--hide');
+  document.querySelector('.timer__icon--start').style.display = 'none';
 
   startTimer();
 
@@ -88,7 +68,7 @@ document.querySelector('.timer__icon').addEventListener('click', () => {
 
   function calculateTimeFraction() {
     const rawTimeFraction = timeLeft / timeLimit;
-    console.log(timeLeft, timeLimit);
+
     return rawTimeFraction - (1 / timeLimit) * (1 - rawTimeFraction);
   }
 
